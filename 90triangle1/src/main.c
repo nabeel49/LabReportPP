9ed3b706f1a80c49093a7606bdd5d219a9de5ddc
Nabeel Farooqui <yournabeel3@gmail.com>
2021-03-17 14:08:39 -0400

    Write a program to print following pattern, with 1 puts statement

---
+++ 90triangle1/src/main.c
@@ -0,0 +1,22 @@
+// making a right angled triangle with one puts
+
+#include <stdio.h>
+
+int main(void)
+{
+  
+puts("#\n\
+##\n\
+###\n\
+####\n\
+#####\n\
+######\n\
+#######\n\
+########\n\
+#########\n\
+##########");
+
+    return 0;
+}
+
+
